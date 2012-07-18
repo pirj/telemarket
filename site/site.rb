96e3dcd633e0477719838374c4c66ef4bcda148c
Phil Pirozhkov <pirj@mail.ru>
2012-07-19 03:24:02 +0400

    slim, layout, reloading, namespace etc

--- site/site.rb
+++ site/site.rb
@@ -1,5 +1,4 @@
 # coding: utf-8
-
 require 'bundler'
 require 'logger'
 Bundler.require
@@ -15,54 +14,29 @@ class Site < Sinatra::Base
 
   configure :development do
     register Sinatra::Reloader
-  # also_reload '/path/to/some/file'
-  # dont_reload '/path/to/other/file'
+    also_reload './*.rb'
   end
 
-# use Rack::Auth::Basic do |username, password|
-#   username == 'admin' && password == 'zue7Eig6ahvo9aqu'
-# end
-
-
-  #     namespace '/blog' do
-  #       get { haml :blog }
-  #       get '/:entry_permalink' do
-  #         @entry = Entry.find_by_permalink!(params[:entry_permalink])
-  #         haml :entry
-  #       end
-  #
-  #       # More blog routes...
-  #     end
+  enable :logging
 
   get '/' do
-    puts "Request #{Time.now}"
-    stream do |out|
-      begin
-        loop do
-          out << "#{Time.now} #{rand(1000)}<br/>"
-          sleep rand 2
+    logger.info "HA"
+    slim :home
+  end
+
+  namespace '/live' do
+    get '/' do
+      stream do |out|
+        begin
+          loop do
+            out << "#{Time.now} #{rand(1000)}<br/>"
+            sleep rand 2
+          end
+        rescue IOError => e
+          # closed
         end
-      rescue IOError => e
-        # closed
       end
     end
   end
-
-  get '/g' do
-    "fsdfsdsd #{Time.now}"
-  end
-
-#   list = []
-#
-#   get '/' do
-#     stream(:keep_open) do |out|
-#       list << out
-#       out.callback { list.delete out }
-#       out.errback do
-#         logger.warn "lost connection"
-#         list.delete out
-#       end
-#     end
-#   end
 
 end
