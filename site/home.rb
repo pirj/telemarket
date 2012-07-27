79e152b9f875234ed53d46ca4aef6b9977e934fc
Phil Pirozhkov <pirj@mail.ru>
2012-07-27 14:55:46 +0400

    comment out

--- site/home.rb
+++ site/home.rb
@@ -4,19 +4,19 @@ class Site < Sinatra::Base
     slim :home
   end
 
-  namespace '/live' do
-    get do
-      stream do |out|
-        begin
-          loop do
-            out << "#{Time.now} #{rand(1000)}<br/>"
-            sleep rand 2
-          end
-        rescue IOError => e
-          # closed
-        end
-      end
-    end
-  end
+  # namespace '/live' do
+  #   get do
+  #     stream do |out|
+  #       begin
+  #         loop do
+  #           out << "#{Time.now} #{rand(1000)}<br/>"
+  #           sleep rand 2
+  #         end
+  #       rescue IOError => e
+  #         # closed
+  #       end
+  #     end
+  #   end
+  # end
 
 end
