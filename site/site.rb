62e1ebb259aefbcf7c249da3ad7e1e59e3db6080
Phil Pirozhkov <pirj@mail.ru>
2012-07-27 14:56:42 +0400

    model path fix

--- site/site.rb
+++ site/site.rb
@@ -15,20 +15,32 @@ class Site < Sinatra::Base
   register Sinatra::Namespace
   register Sinatra::Flash
 
-  use Rack::Session::Pool
+  use Rack::Session::Cookie #, 'paimoo4Odoo3FeWiovaiVi9iYi0PoceeHaesho3azeiy3aVuahri5Shibio6ohCh'
   use Rack::Protection
+
+  use OmniAuth::Builder do
+    provider :identity, :fields => [:email]
+  end
+
+  error OmniAuth::Error do
+    403
+  end
+
+  [403, 404, 405, 500].each do |code|
+    error code do
+      slim "errors/#{code}"
+    end
+  end
 
   DataMapper.setup(:default, "sqlite://#{Dir.pwd}/development.db")
 
   configure :development do
     register Sinatra::Reloader
     also_reload './*.rb'
-    also_reload './models.rb'
+    also_reload './models/*.rb'
   end
 
   enable :logging
-  # enable :authentication
-
   set :sessions, true
 
   not_found do
@@ -38,5 +50,4 @@ class Site < Sinatra::Base
   error do
     'Произошло нечто ужасное: ' + env['sinatra.error'].name
   end
-
 end
