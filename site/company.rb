82fb5190cefe22d1dd92b9cea22325953a956897
Phil Pirozhkov <pirj@mail.ru>
2012-07-27 14:55:26 +0400

    company internal

---
+++ site/company.rb
@@ -0,0 +1,11 @@
+# class Site < Sinatra::Base
+#   namespace '/company' do
+#     before { authenticate }
+
+#     get do
+#       authorize! '/enter'
+
+#       slim :company
+#     end
+#   end
+# end
