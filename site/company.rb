0c598d8365c091d6cb25a6a9330e04599c7676af
Phil Pirozhkov <pirj@mail.ru>
2012-08-03 21:49:41 +0400

    com[pany plans and grps

--- site/company.rb
+++ site/company.rb
@@ -1,10 +1,5 @@
 # coding: utf-8
 class Site < Sinatra::Base
-  get '/company' do
-    @company = current_user.employee.company
-    slim :'company/index'
-  end
-
   get '/company/register' do
     slim :'company/register'
   end
@@ -17,5 +12,20 @@ class Site < Sinatra::Base
     session[:user_id] = identity.id
     flash[:info] = "Добро пожаловать!"
     redirect '/company'
+  end
+
+  get '/company' do
+    @company = current_user.employee.company
+    slim :'company/index'
+  end
+
+  get '/company/plans' do
+    @company = current_user.employee.company
+    slim :'company/plans'
+  end
+
+  get '/company/target-groups' do
+    @company = current_user.employee.company
+    slim :'company/target-groups'
   end
 end
