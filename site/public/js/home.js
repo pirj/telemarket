d6d09ed1669067dd681150ce58eeba36044d67bb
Phil Pirozhkov <pirj@mail.ru>
2012-10-29 03:59:15 +0400

    landing page redesign

--- site/public/js/home.js
+++ site/public/js/home.js
@@ -1,16 +1,3 @@
 $(function() {
-  var BV = new $.BigVideo();
-  BV.init();
-  BV.show('/lightbulb.mp4',{ambient:true});
 
-  setTimeout(function(){
-    $('#transparent').addClass('loaded');
-  }, 1000);
-
-  $('#da-slider').cslider({
-    //bgincrement : 0,
-    autoplay    : true,
-    interval    : 15000,
-    autoHover   : true
-  });
-});
+})
